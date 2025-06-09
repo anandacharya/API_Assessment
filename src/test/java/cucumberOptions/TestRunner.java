c51f443be4b994a7e915e21f47f5bd3d300bedf0
Anand Acharya <anandacharya.84@gmail.com>
2025-06-10 09:32:34 +1000

    updated for report generation

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -5,13 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {
-                "pretty",
-                "json:target/cucumber.json",
-                "html:target/cucumber-html-report"
-        },
-        features = {"src/test/java/features"},
-        glue={"stepDefinitions"})
+@CucumberOptions(plugin = "json:target/jsonReports/cucumber-report.json",
+        features = "src/test/java/features",
+        glue="stepDefinitions")
 public class TestRunner {
 }
